09d41c112a63abcc82a252f3c42489a2ccd4b305
OnlyDropped <asheburenkov@yandex.ru>
2022-09-24 03:42:55 +0300

    added new sections

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ const menu = document.querySelector('.menu');
 btn.addEventListener('click', ()=>{
   btn.classList.toggle('header-top__btn--active')
 })
+
+$(function() {
+
+	$('.price__file').styler({
+    fileBrowse: 'Выбрать',
+  });
+
+});
+
+
+$('.advantage-slider').slick({
+  cssEase: 'ease-out',
+  slidesToShow: 2,
+  slidesToScroll: 1
+})
